e6d49c15be1d5a3eb384152187d877035b884785
FunkiDunki <nicholashotelling@gmail.com>
2023-11-05 10:58:24 -0800

    added example flow 2

--- src/api/groups.py
+++ src/api/groups.py
@@ -43,3 +43,71 @@ def post_deliver_bottles(group_id: int, user_id: int):
         ).scalar_one()
    
     return {'id': id}
+
+class PurchaseInfo(BaseModel):
+    description: str
+    price: float
+
+@router.post("/{group_id}/users/{user_id}/purchases")
+def post_group_purchase(group_id: int, user_id: int, purchase: PurchaseInfo):
+
+    with db.engine.begin() as connection:
+        #create purchase
+        purchase_id = connection.execute(
+            sqlalchemy.text('''INSERT into purchases (group_id, buyer, description, price)
+                            VALUES (:gid, :uid, :description, :price)
+                            RETURNING id'''),
+            {
+                'gid': group_id,
+                'uid': user_id,
+                'description': purchase.description,
+                'price': purchase.price
+            }
+        ).scalar_one()
+
+        #create transactions
+        transaction_ids = connection.execute(
+            sqlalchemy.text(
+                '''
+                WITH groupmates as (
+                    SELECT DISTINCT user_id
+                    FROM users_to_group
+                    WHERE group_id = :gid AND NOT user_id = :uid
+                ),
+                total_groupmates as (
+                    SELECT COUNT(*) as total
+                    FROM groupmates
+                )
+                INSERT INTO transactions
+                (from_user, to_user, value)
+                SELECT :uid, groupmates.user_id, ROUND(:price / total_groupmates.total,2)
+                FROM groupmates CROSS JOIN total_groupmates
+                RETURNING id
+                '''
+            ),
+            {
+                'gid': group_id,
+                'uid': user_id,
+                'price': -purchase.price
+            }
+        ).all()
+
+        transaction_ids = [id.id for id in transaction_ids]
+
+        #create purchase_transactions
+        for id in transaction_ids:
+            connection.execute(
+                sqlalchemy.text(
+                    '''
+                    INSERT INTO purchase_transactions
+                    (purchase_id, transaction_id)
+                    VALUES
+                    (:pid, :tid)
+                    '''
+                ),
+                {
+                    'pid': purchase_id,
+                    'tid': id
+                }
+            )
+    return "OK"
--- src/api/users.py
+++ src/api/users.py
@@ -29,3 +29,31 @@ def post_deliver_bottles(newuser: User):
    
     return {'new_user_id': id}
 
+@router.get("/{user_id}/balances/{other_user_id}")
+def get_user_balance(user_id: int, other_user_id: int):
+    with db.engine.begin() as connection:
+        result = connection.execute(
+            sqlalchemy.text(
+                '''
+                WITH outbound as (
+                    SELECT COALESCE(SUM(value), 0) amount
+                    FROM transactions
+                    WHERE from_user = :uid1 AND to_user = :uid2
+                ),
+                inbound as (
+                    SELECT COALESCE(SUM(value), 0) amount
+                    FROM transactions
+                    WHERE to_user = :uid1 AND from_user = :uid2
+                )
+                SELECT (inbound.amount - outbound.amount) as amount
+                FROM inbound CROSS JOIN outbound
+                '''
+            ),
+            {
+                'uid1': user_id,
+                'uid2': other_user_id
+            }
+        ).scalar_one()
+    return {"Balance": result}
+
+
