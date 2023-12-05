57eb8c404f3c8dd920f35e122f411fdb02e2ede9
NickPerlich <nicholasperlich2003@gmail.com>
2023-12-04 21:03:08 -0800

    added try except to all endpoints

--- src/api/users.py
+++ src/api/users.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import sqlalchemy
 from src import database as db
 from operator import itemgetter
+from sqlalchemy.exc import DBAPIError
 
 router = APIRouter(
     prefix="/users",
@@ -22,123 +23,137 @@ class Payment(BaseModel):
 
 @router.post("/create_user")
 def create_user(new_user: User):
-    with db.engine.begin() as connection:
-        id = connection.execute(sqlalchemy.text("""INSERT INTO users (name, email, phone) 
-                                                VALUES (:name, :email, :phone) RETURNING id"""), {
-                                                    'name': new_user.name,
-                                                    'email': new_user.email,
-                                                    'phone': new_user.phone
-                                                }).scalar_one()
-   
-    return {'new_user_id': id}
+    try:
+        with db.engine.begin() as connection:
+            id = connection.execute(sqlalchemy.text("""INSERT INTO users (name, email, phone) 
+                                                    VALUES (:name, :email, :phone) RETURNING id"""), {
+                                                        'name': new_user.name,
+                                                        'email': new_user.email,
+                                                        'phone': new_user.phone
+                                                    }).scalar_one()
+    
+        return {'new_user_id': id}
+    except DBAPIError as error:
+        print(f"Error returned: <<<{error}>>>")
 
 @router.get("/{user_id}/balances/{other_user_id}")
 def get_user_balance(user_id: int, other_user_id: int):
-    with db.engine.begin() as connection:
-        result = connection.execute(
-            sqlalchemy.text(
-                '''
-                WITH outbound as (
-                    SELECT COALESCE(SUM(value), 0) amount
-                    FROM transactions
-                    WHERE from_user = :uid1 AND to_user = :uid2
+    try:
+        with db.engine.begin() as connection:
+            result = connection.execute(
+                sqlalchemy.text(
+                    '''
+                    WITH outbound as (
+                        SELECT COALESCE(SUM(value), 0) amount
+                        FROM transactions
+                        WHERE from_user = :uid1 AND to_user = :uid2
+                    ),
+                    inbound as (
+                        SELECT COALESCE(SUM(value), 0) amount
+                        FROM transactions
+                        WHERE to_user = :uid1 AND from_user = :uid2
+                    )
+                    SELECT (inbound.amount - outbound.amount) as amount
+                    FROM inbound CROSS JOIN outbound
+                    '''
                 ),
-                inbound as (
-                    SELECT COALESCE(SUM(value), 0) amount
-                    FROM transactions
-                    WHERE to_user = :uid1 AND from_user = :uid2
-                )
-                SELECT (inbound.amount - outbound.amount) as amount
-                FROM inbound CROSS JOIN outbound
-                '''
-            ),
-            {
-                'uid1': user_id,
-                'uid2': other_user_id
-            }
-        ).scalar_one()
-    return {"Balance": result}
+                {
+                    'uid1': user_id,
+                    'uid2': other_user_id
+                }
+            ).scalar_one()
+        return {"Balance": result}
+    except DBAPIError as error:
+        print(f"Error returned: <<<{error}>>>")
 
 @router.post("/{user_id}/pay/{other_user_id}")
 def post_payment(user_id: int, other_user_id: int, payment: Payment):
-
-    with db.engine.begin() as connection:
-        id = connection.execute(sqlalchemy.text("""INSERT INTO transactions (from_user, to_user, value)
-                                                    VALUES (:user1, :user2, ROUND(:payment, 2))
-                                                    RETURNING id
-                                                """), {
-                                                    'user1': user_id,
-                                                    'user2': other_user_id,
-                                                    'payment': payment.amount
-                                                }).scalar_one()
-        connection.execute(sqlalchemy.text("""INSERT INTO settlements (description, transaction_id)
-                                                VALUES (:desc, :tid)"""), {
-                                                    'desc': payment.description,
-                                                    'tid': id
-                                                })
-        
-    return {"Amount paid": payment.amount}
+    try:
+        with db.engine.begin() as connection:
+            id = connection.execute(sqlalchemy.text("""INSERT INTO transactions (from_user, to_user, value)
+                                                        VALUES (:user1, :user2, ROUND(:payment, 2))
+                                                        RETURNING id
+                                                    """), {
+                                                        'user1': user_id,
+                                                        'user2': other_user_id,
+                                                        'payment': payment.amount
+                                                    }).scalar_one()
+            connection.execute(sqlalchemy.text("""INSERT INTO settlements (description, transaction_id)
+                                                    VALUES (:desc, :tid)"""), {
+                                                        'desc': payment.description,
+                                                        'tid': id
+                                                    })
+            
+        return {"Amount paid": payment.amount}
+    except DBAPIError as error:
+        print(f"Error returned: <<<{error}>>>")
 
 @router.get("/{user_id}/balance_breakdown")
 def get_balance_breakdown(user_id: int):
-    with db.engine.begin() as connection:
-        result = connection.execute(
-            sqlalchemy.text(
-                '''
-                WITH outbound AS (
-                SELECT SUM(value) AS amount, to_user AS user_id
-                FROM transactions
-                WHERE from_user = 101
-                GROUP BY to_user
-                ),
-                inbound AS (
-                    SELECT SUM(value) AS amount, from_user AS user_id
+    try:
+        with db.engine.begin() as connection:
+            result = connection.execute(
+                sqlalchemy.text(
+                    '''
+                    WITH outbound AS (
+                    SELECT SUM(value) AS amount, to_user AS user_id
                     FROM transactions
-                    WHERE to_user = 101
-                    GROUP BY from_user
-                )
-                SELECT user_id, amount
-                FROM outbound
-                WHERE amount > 0
+                    WHERE from_user = 101
+                    GROUP BY to_user
+                    ),
+                    inbound AS (
+                        SELECT SUM(value) AS amount, from_user AS user_id
+                        FROM transactions
+                        WHERE to_user = 101
+                        GROUP BY from_user
+                    )
+                    SELECT user_id, amount
+                    FROM outbound
+                    WHERE amount > 0
 
-                UNION
+                    UNION
 
-                SELECT user_id, -amount AS amount
-                FROM inbound
-                WHERE amount < 0
-                '''
-            ),
-            {
-                'uid1': user_id
-            }
-        ).fetchall()
-    return {"Balance Breakdown": result}
+                    SELECT user_id, -amount AS amount
+                    FROM inbound
+                    WHERE amount < 0
+                    '''
+                ),
+                {
+                    'uid1': user_id
+                }
+            ).fetchall()
+        return {"Balance Breakdown": result}
+    except DBAPIError as error:
+        print(f"Error returned: <<<{error}>>>")
 
 @router.post("/{user_id}/resolve_balance")
 def post_resolve_balance(user_id: int):
-    with db.engine.begin() as connection:
-        result = connection.execute(
-            sqlalchemy.text(
-                '''
-                SELECT to_user AS user, SUM(value) AS amount
-                FROM transactions
-                WHERE from_user = 101
-                GROUP BY user
-                '''
-            ),
-            {
-                'uid1': user_id
-            }
-        ).fetchall()
+    try:
+        with db.engine.begin() as connection:
+            result = connection.execute(
+                sqlalchemy.text(
+                    '''
+                    SELECT to_user AS user, SUM(value) AS amount
+                    FROM transactions
+                    WHERE from_user = 101
+                    GROUP BY user
+                    '''
+                ),
+                {
+                    'uid1': user_id
+                }
+            ).fetchall()
 
-        for i in result:
-            id = connection.execute(sqlalchemy.text("""INSERT INTO transactions (from_user, to_user, value)
-                                                    VALUES (:user1, :user2, ROUND(:payment, 2))
-                                                    RETURNING id
-                                                """), {
-                                                    'user1': user_id,
-                                                    'user2': i[0],
-                                                    'payment': i[1]
-                                                }).scalar_one()
+            for i in result:
+                id = connection.execute(sqlalchemy.text("""INSERT INTO transactions (from_user, to_user, value)
+                                                        VALUES (:user1, :user2, ROUND(:payment, 2))
+                                                        RETURNING id
+                                                    """), {
+                                                        'user1': user_id,
+                                                        'user2': i[0],
+                                                        'payment': i[1]
+                                                    }).scalar_one()
 
-    return {"All balances settled"}
+        return {"All balances settled"}
+    except DBAPIError as error:
+        print(f"Error returned: <<<{error}>>>")
