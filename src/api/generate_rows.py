a0ad5b8b350d1b4e24227d229a50eb0178cb4705
PauMinguet <pablitumm@gmail.com>
2023-12-04 18:50:59 -0800

    queries

--- src/api/generate_rows.py
+++ src/api/generate_rows.py
@@ -1,5 +1,14 @@
 from random_username import generate
 import random
+import sqlalchemy
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+#from src.api import auth
+import sqlalchemy
+from src import database as db
+from operator import itemgetter
+from sqlalchemy.exc import DBAPIError
+
 
 def spam_users(n):
     usernames = generate.generate_username(n)
@@ -14,8 +23,28 @@ def spam_users(n):
     res = []
     for i in range(n):
         res.append([usernames[i], emails[i], phones[i]])
+
+    query = "INSERT INTO users (name, email, phone) VALUES \n"
+
+    for i in range(n):
+        query += "('"+ res[i][0] +"', '"+ res[i][1] +"', '"+ str(res[i][2]) +"'), \n"        
+
+    query = query[:-3] + ";"
+
+
+    print(query)
+
+
+    #with db.engine.begin() as connection:
+    #    id = connection.execute(sqlalchemy.text(query))
     
     return res
+
+
+
+
+
+
 
 def spam_groups(n, users):
     group_names = [' Roommates', ' Band', ' Friends', ' Partners in Crime', ' Travel Buddies', ' Booze Buddies', ' Grocerie Splitters']
@@ -29,7 +58,27 @@ def spam_groups(n, users):
         name = users[userindex][0] + "'s" + group_names[groupindex]
         description = group_descriptions[groupindex]
         groups.append([name, description, userindex])
+
+    query = "INSERT INTO users_to_group (user_id, group_id) VALUES \n"
+
+    for i in range(n):
+        query += "('"+ groups[i][0] +"', '"+ groups[i][1] +"'), \n"        
+
+    query = query[:-3] + ";"
+
+    print(query)
+
+    #with db.engine.begin() as connection:
+    #    id = connection.execute(sqlalchemy.text(query))
+
     return groups
+
+
+
+
+
+
+
 
 def spam_users_in_group(n, groups, users):
 
@@ -43,7 +92,27 @@ def spam_users_in_group(n, groups, users):
 
         res.append([userindex, groupindex])
 
+    
+    query = "INSERT INTO groups (name, description) VALUES \n"
+
+    for i in range(n):
+        query += "("+ str(res[i][0]) +", "+ str(res[i][1]) +"), \n"        
+
+    query = query[:-3] + ";"
+
+    print(query)
+
+    #with db.engine.begin() as connection:
+    #    id = connection.execute(sqlalchemy.text(query))
+
     return res
+
+
+
+
+
+
+
 
 def spam_transactions(n, users_per_groups):
     
@@ -55,7 +124,22 @@ def spam_transactions(n, users_per_groups):
         price = random.randint(10, 1000)
         index = random.randint(0, len(users_per_groups)-1)
         description = descriptions[random.randint(0, len(descriptions)-1)]
-        transactions.append(users_per_groups[index][1], users_per_groups[index][0], description, price)
+        transactions.append([users_per_groups[index][1], users_per_groups[index][0], description, price])
+
+
+    query = "INSERT into purchases (group_id, buyer, description, price) VALUES (:gid, :uid, :description, :price) \n"
+
+    for i in range(n):
+        query += "("+ str(transactions[i][0]) +", "+ str(transactions[i][1]) +", "+ str(transactions[i][2]) +", "+ str(transactions[i][3]) +"), \n"        
+
+    query = query[:-3] + ";"
+
+    print(query)
+
+    #with db.engine.begin() as connection:
+    #    id = connection.execute(sqlalchemy.text(query))
+
+
 
     return transactions
 
@@ -68,5 +152,3 @@ groups = spam_groups(50000, users)
 users_per_groups = spam_users_in_group(250000, groups, users)
 
 transactions = spam_transactions(600000, users_per_groups)
-
-print()
